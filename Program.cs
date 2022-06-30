57fdbb677187a05053114ffc4bcff317af542f53
Maxspar2002 <maxspar2002@yandex.ru>
2022-06-30 17:52:13 +0300

    Добавлено решение Задачи 2

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// Домашнее задание к семинару №1
+//
+// Задача 2
+
+int num1, num2;
+Console.Write("Введите первое число: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+   Console.WriteLine("Большее число - " + num1);
+   Console.WriteLine("Меньшее число - " + num2);
+}
+else
+{
+    if (num1 < num2)
+    {
+        Console.WriteLine("Большее число - " + num2);
+        Console.WriteLine("Меньшее число - " + num1);
+    }
+    else
+        Console.WriteLine("Введенные числа - равны");
+}    
+
